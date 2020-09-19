dc3b5b3f3336c72bf711b36d209685cfdec0acd4
Robin <robin23jac@gmail.com>
2020-09-19 13:18:47 +0200

    feat: Add typescript workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
 export default [
@@ -13,7 +14,8 @@ export default [
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			commonjs(), // so Rollup can convert `ms` to an ES module
+			typescript() // so Rollup can convert TypeScript to JavaScript
 		]
 	},
 
@@ -26,6 +28,9 @@ export default [
 	{
 		input: 'src/main.js',
 		external: ['ms'],
+		plugins: [
+			typescript() // so Rollup can convert TypeScript to JavaScript
+		],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
